e23851449c84f229569a1056f2bc05acc3155202
Luyaochen1 <lychen9999@gmail.com>
2023-03-01 09:39:53 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # setup coonfiguration data here
 import os
 
-available_dbs = {'PSQL_MIMIC': ["userid:password@192.168.0.199:5432/mimic","mimiciii"],
+available_dbs = {'PSQL_MIMIC': ["postgresql://userid:password@192.168.0.199:5432/mimic","mimiciii"],
                 'LOCAL_TEXT_FILES': 'LOCAL_TEXT_FILES'
         }
  
